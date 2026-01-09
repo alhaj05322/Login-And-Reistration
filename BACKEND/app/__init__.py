d09374c4021eaecd20c48cf538bcef16b83f49c4
Alhaj Musa <alhaj05322@gmail.com>
2026-01-08 19:49:36 -0800

    Create Flask configuration file

--- BACKEND/app/__init__.py
+++ BACKEND/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+from app.extensions import db
 import os
 
 
@@ -8,6 +9,11 @@ def create_app(config_class: type = Config):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object(config_class)
+    db.init_app(app)
+
+    @app.get("/")
+    def home():
+        return "Welcome flask"
 
 
     return app
